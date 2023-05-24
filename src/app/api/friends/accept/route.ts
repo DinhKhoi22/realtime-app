f13bef1bcd5cd189b06d05ec073404f9b01f30af
DinhKhoi22 <111755052+DinhKhoi22@users.noreply.github.com>
2023-05-24 16:46:58 +0700

    Update route.ts

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -45,7 +45,7 @@ export async function POST (req: Request) {
             pusherServer.trigger(toPusherKey(`user:${session.user.id}:friends`), 'new_friend', friend),
             db.sadd(`user:${session.user.id}:friends`, idToAdd),
             db.sadd(`user:${idToAdd}:friends`, session.user.id),
-            db.srem(`user:${session.user.id}:incoming_friend_request`, idToAdd),
+            db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd),
         ])
         
         return new Response('OK')

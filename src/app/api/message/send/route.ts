b3e8c5bacb7058bf6825a9653046d14acec78ec6
DinhKhoi22 <111755052+DinhKhoi22@users.noreply.github.com>
2023-05-29 17:29:28 +0700

    await in chat file  pusherServer.trigger

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -44,9 +44,9 @@ export async function POST (req: Request) {
         const message = messageValidator.parse(messageData);
 
         // notify all connected chatroom clients
-        pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message);
+        await pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message);
 
-        pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {
+        await pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {
             ...message,
             senderImg: sender.image,
             senderName: sender.name,
